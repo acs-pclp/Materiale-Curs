60c2ad536d54abb6463a8b60b42855d9ff1314a8
Remus Neatu <neaturemus07@gmail.com>
2019-09-26 23:32:50 +0300

    Curs01: exemple in C

---
+++ 2019-2020/Curs01/C/01-helloworld.c
@@ -0,0 +1,13 @@
+/* Programarea Calculatoarelor CA, 2019-2020
+
+   Programul afiseaza mesajul "Hello World". */
+
+#include <stdio.h> // pentru a folosi printf si scanf
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
+
+/* compile: gcc 01-helloworld.c -o helloworld
+   run    : ./helloworld */
---
+++ 2019-2020/Curs01/C/04-min_max_if.c
@@ -0,0 +1,33 @@
+/* Programarea Calculatoarelor CA, 2019-2020
+
+   Programul calculeaza minimul si maximul dintre 2 numere
+   folosind "if" */
+
+#include <stdio.h>
+
+int main() {
+    int a, b, min, max;
+    scanf("%d%d", &a, &b);
+
+    if (a > b) {
+        max = a;
+        min = b;
+    } else {
+        max = b;
+        min = a;
+    }
+
+    printf("max: %d\nmin: %d\n", max, min);
+    return 0;
+}
+
+/* compile: gcc 04-min_max_if.c -o minmax
+   run    : ./minmax
+
+   Exemplu:
+
+   input:
+   -9 -3
+   output:
+   max: -3
+   min: -9   */
