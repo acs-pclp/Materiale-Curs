3807ab7e0dd549f623a5b4cc8dff426199898c98
Remus-Mihai Neatu <neaturemus07@gmail.com>
2019-10-23 14:38:48 +0300

    Curs02: exemple in C

---
+++ 2019-2020/Curs02/C/02-interschimbare.c
@@ -0,0 +1,46 @@
+/* Programarea Calculatoarelor CA, 2019-2020
+
+   Programul realizeaza interschimbarea a 2 numere prin diferite metode */
+
+#include <stdio.h>
+
+int main() {
+    // interschimbare cu auxiliar
+    int a, b, aux;
+    scanf("%d%d", &a, &b);
+    aux = a;
+    a = b;
+    b = aux;
+    printf("%d %d\n", a, b);
+
+    // interschimbare cu xor
+    int x, y;
+    scanf("%d%d", &x, &y);
+    x = x ^ y;
+    y = x ^ y;
+    x = x ^ y;
+    printf("%d %d\n", x, y);
+
+    // interschimbare cu adunari
+    int m, n;
+    scanf("%d%d", &m, &n);
+    m = m + n;
+    n = m - n;
+    m = m - n;
+    printf("%d %d\n", m, n);
+
+    return 0;
+}
+
+/* compile: gcc 02-interschimbare.c -o interschimbare
+   run    : ./interschimbare
+
+   Exemplu:
+   input : 2 5
+   output: 5 2
+
+   input : 13 -4
+   output: -4 13
+
+   input : 12 13
+   output: 13 12        */
---
+++ 2019-2020/Curs02/C/03-operatori_incrementare.c
@@ -0,0 +1,20 @@
+/* Programarea Calculatoarelor CA, 2019-2020
+   Exemple cu operatori de incrementare / decrementare pre și post fixată  */ 
+
+/*  Operatorul de incrementare este ++, iar cel de decremetare este --
+	Fiecare dintre acestia pot fi pre si post fixati;
+	Pre : ++a;
+	Post: b--;
+	In cazul operatorului pre fixat, mai intai se realizeaza incrementare,
+	respectiv decrementarea, iar apoi se lucreaza cu noua valoare
+	In cazul operatorului post fixat, mai intai se lucreaza cu valoarea, iar
+	abia dupa este incrementata, respectiv decrementata
+	Ex : x = 7;
+		 ++x; (x = 8)
+		 x++; (x = 9);
+
+		 a = 2;
+		 b = a++; => b = 2, a = 3;
+
+		 c = 14;
+		 d = --c; => c = 13, d = 13;  */
