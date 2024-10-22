624899c7ee7c1441138e58208311292e85fe7a4f
Radu Nichita <radunichita99@gmail.com>
2024-10-22 23:03:21 +0200

    C06/C: fix matrix example
    Signed-off-by: Radu Nichita <radunichita99@gmail.com>

--- Curs05/C/06-mul_matrix.c
+++ Curs05/C/06-mul_matrix.c
@@ -6,13 +6,38 @@
 #include <stdio.h>
 #define NMAX 25
 
+void multiply_matrix(int n, int m, int p, int a[NMAX][NMAX], int b[NMAX][NMAX], int c[NMAX][NMAX]) {
+	/* Declarăm o matrice auxiliară, putem avea apeluri de forma
+	multiply_matrix(a, a, a) <=> a = a * a */
+	int tmp[NMAX][NMAX] = {};
+
+	// calculez fiecare element al matricei după formula
+	// tmp[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j]] + ... a[i][p - 1] * a[p - 1][j]
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			tmp[i][j] = 0;
+			for (int k = 0; k < p; k++) {
+				tmp[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+
+	// salvez valorile obținute în matricea c
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			c[i][j] = tmp[i][j];
+		}
+	}
+}
+
 int main(void)
 {
 	/* cele trei matrice și dimensiunile */
 	int a[NMAX][NMAX], b[NMAX][NMAX], c[NMAX][NMAX], n, m, p;
 
-	/* citim matricile a și b de dimensiuni n x p și p x m */
+	/* citim n, p, și matricile a și b de dimensiuni n x p și p x m */
 	scanf("%d%d%d", &n, &m, &p);
+
 	printf("Matricea a de dimensiuni %d x %d:\n", n, p);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < p; j++) {
@@ -27,17 +52,10 @@ int main(void)
 	}
 
 	/* înmulțirea celor 2 matrici */
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			c[i][j] = 0;
-			for (int k = 0; k < p; k++) {
-				c[i][j] += a[i][k] * b[k][j];
-			}
-		}
-	}
+	multiply_matrix(n, m, p, a, b, c);
 
-	/* afișarea rezultatului */
-	printf("Matricea produc c = a x b de dimensiuni %d x %d:\n", n, m);
+	/* afișarea rezultatului înmulțirii celor două matrici */
+	printf("Matricea produs c = a x b de dimensiuni %d x %d:\n", n, m);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			printf("%5d ", c[i][j]);
@@ -61,7 +79,7 @@ int main(void)
 	7 6 5
 
 	output :
-	Matricea produc c = a x b de dimensiuna 2 x 3:
+	Matricea produs c = a x b de dimensiuna 2 x 3:
 	36    41    38
 	46    53    48
 	explicatie :
